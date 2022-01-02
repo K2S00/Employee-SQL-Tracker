d3df5fe469b265fe45ec88391add36af0a192c3b
[Rebecca <rdearry@gmail.com>
2022-01-01 23:26:02 -0500

    [manager query]

--- utils/queries/employeeQueries.js
+++ utils/queries/employeeQueries.js
@@ -24,4 +24,28 @@ let queryEmployees = function() {
         })
     };
     
-    
+    //query for manager 
+   // Query employees by Manager
+let queryEmployeesByManager = function(managerId) {
+    const sql = `SELECT employees.id,
+                        employees.first_name,
+                        employees.last_name,
+                        roles.title,
+                        departments.name AS department,
+                        roles. salary,
+                        CONCAT(m.first_name, ' ', m.last_name) AS manager
+                FROM employees
+                LEFT JOIN roles ON employees.role_id = roles.id
+                LEFT JOIN departments ON roles.department_id = departments.id
+                LEFT JOIN employees m ON employees.manager_id = m.id
+                WHERE employees.manager_id = ?`;
+
+    return new Promise((resolve, reject) => {
+        db.query(sql, managerId, (err, rows) => {
+            if (err) {
+                reject({ message: err.message });
+            }
+            resolve(rows);
+        });
+    })
+}; 
