29558c4579b515b4e555f93617333db5ddd1af27
[Rebecca <rdearry@gmail.com>
2022-01-02 15:09:56 -0500

    [roles complete]

--- utils/queries/roleQueries.js
+++ utils/queries/roleQueries.js
@@ -33,3 +33,62 @@ let queryBudget = function(departmentId) {
                 });
             })
         };
+
+// INSERTS role into roles table
+let addRole = function(title, salary, departmentId) {
+    const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`;
+    let params = [title, salary, departmentId];
+    
+    return new Promise((resolve, reject) => {
+        db.query(sql, params, (err, result) => {
+            if (err) {
+                reject({ error: err.message });
+            }
+            resolve({
+                message: '\n Congratulations! You successfully added a role\n',
+                data: result
+            });
+        });
+    });
+};
+
+// DELETE role from roles table
+let deleteRole = function(id) {
+    const sql = `DELETE FROM roles WHERE id = ?`;
+    return new Promise((resolve, reject) => {
+        db.query(sql, id, (err, result) => {
+            if (err) {
+                reject({ error: err.message });
+            }
+            resolve({
+                message: '\nSuccessfully deleted role\n',
+                changes: result.affectedRows,
+                id: id
+            });
+        });
+    });
+}
+
+// DELETE role from roles table
+let deleteRole = function(id) {
+    const sql = `DELETE FROM roles WHERE id = ?`;
+    return new Promise((resolve, reject) => {
+        db.query(sql, id, (err, result) => {
+            if (err) {
+                reject({ error: err.message });
+            }
+            resolve({
+                message: '\nCongratulations! You successfully deleted role\n',
+                changes: result.affectedRows,
+                id: id
+            });
+        });
+    });
+}
+
+module.exports = {
+    queryRoles,
+    queryBudget,
+    addRole,
+    deleteRole
+}
