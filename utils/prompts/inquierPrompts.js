669681581d940074e6ad07fe460ed84258565a24
[Rebecca <rdearry@gmail.com>
2022-01-02 15:32:21 -0500

    [main menu]

--- utils/prompts/inquierPrompts.js
+++ utils/prompts/inquierPrompts.js
@@ -19,3 +19,33 @@ const { queryEmployees,
         updateEmployeeRole,
         updateEmployeeManager,
         deleteEmployee } = require('../queries/employeeQueries');
+
+let mainMenu = async function() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "mainMenu",
+            message: "Main Menu",
+            choices: [
+                "View all departments",
+                    "View all roles",
+                    "View all employees",
+                    "Add a department",
+                    "Add a role",
+                    "Add an employee",
+                    "Delete a department",
+                    "Delete a role",
+                    "Delete an employee",
+                    "Update an employee role",
+                    "Update an employee manager",
+                    "View employees by manager",
+                    "View employees by department",
+                    "View department budget",
+                    "Exit"
+            ]
+        }
+    ])
+
+
+    
+
