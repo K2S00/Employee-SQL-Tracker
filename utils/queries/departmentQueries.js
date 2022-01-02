f0cee87cdb42e08fe27ed182ea3856f5d26cf7d9
[Rebecca <rdearry@gmail.com>
2022-01-01 21:24:41 -0500

    [return department table]

--- utils/queries/departmentQueries.js
+++ utils/queries/departmentQueries.js
@@ -1,6 +1,17 @@
 const db = require('../../db/connections');
 
 //query/return deparment table 
-let queryDepartments = fuction (){
-    const sql = 
-}
+let queryDepartments = function() {
+    const sql = `SELECT * FROM departments`;
+
+    return new Promise((resolve, reject) => {
+        db,query(sql, (err, rows) =>{
+            if (err) {
+                reject(err);
+            }
+            resolve(rows);
+        });
+    })
+};
+
+//inserts into department table 
